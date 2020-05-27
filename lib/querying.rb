e89c05184c1280622be9017921d658cb471810e2
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-26 22:30:36 -0400

    lab completed

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,56 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  <<-SQL
+   SELECT title, year
+   FROM books
+   WHERE series_id = 1
+   ORDER BY year ASC
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  <<-SQL
+  SELECT name, motto
+  FROM characters
+  ORDER BY LENGTH(motto) DESC LIMIT 1
+  SQL
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  <<-SQL
+   SELECT MAX(species), COUNT(species)
+   FROM characters
+   WHERE species = 'human'
+  SQL
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  <<-SQL
+   SELECT authors.name, subgenres.name
+   FROM authors
+   INNER JOIN series
+   ON authors.id = series.author_id
+   JOIN subgenres ON series.subgenre_id = subgenres.id
+  SQL
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL
+    SELECT series.title
+    FROM series
+    INNER JOIN characters
+    ON series.author_id = characters.author_id
+    WHERE characters.species = 'human' LIMIT 1
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  <<-SQL
+    SELECT characters.name, COUNT(*)
+    FROM character_books
+    JOIN characters
+    ON character_books.character_id = characters.id
+    GROUP BY characters.name
+    ORDER BY COUNT(*) DESC, characters.name ASC
+  SQL
 end
